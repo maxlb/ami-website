6379b9e9d5717f4caa9e57f6ffcc0dd04771d8cf
Maxime LB <maxime.lebail@orange.fr>
2018-11-12 17:03:35 +0100

    fin homepage + deco + layout

--- config/views.js
+++ config/views.js
@@ -21,7 +21,7 @@ module.exports.views = {
   * Extension to use for your views. When calling `res.view()` in an action, *
   * you can leave this extension off. For example, calling                   *
   * `res.view('homepage')` will (using default settings) look for a          *
-  * `views/homepage.ejs` file.                                               *
+  * `views/dashboard.ejs` file.                                               *
   *                                                                          *
   ***************************************************************************/
 
