7aeef95a799c35d1a39697597f6dc98211d39500
Maxime LB <maxime.lebail@orange.fr>
2019-02-07 21:49:56 +0100

    stats - evolution inscit 12 mois

--- api/controllers/StatistiquesController.js
+++ api/controllers/StatistiquesController.js
@@ -221,9 +221,40 @@ module.exports = {
 
     async getInscritsParMois(req, res) {
 
+        var mois = ["Jan.", "Fév.", "Mars", "Avril", "Mai", "Juin", "Juil.", "Aout", "Sept.", "Oct.", "Nov.", "Déc."];
+        var indexMoisEnCours = new Date().getMonth();
+        var anneeEnCours = new Date().getFullYear()
+        var labelsMois = [];
+        var valeursMois = [];
+
+        for (let i = 0 ; i < 12 ; i++) {
+            /* Mois à afficher */
+            var anneeGraph = anneeEnCours;
+            var indexMoisGraph = indexMoisEnCours - i;
+            if (indexMoisGraph < 0) {
+                indexMoisGraph += 12;
+                anneeGraph = anneeEnCours - 1
+            }
+            var debutMois = new Date(anneeGraph, indexMoisGraph, 1)
+            var finMois = new Date(anneeGraph, indexMoisGraph + 1, 1)
+            var nbBenefs = await Beneficiaire.count({ dateInscription: { '>=': debutMois }, dateInscription: { '<': finMois } });
+
+            /* Ajout du mois dans le labels */
+            labelsMois.push(mois[indexMoisGraph] + " " +  anneeGraph);
+            valeursMois.push(nbBenefs);
+        }
+
+        /* On inverse l'ordre pour que ça soit chronologique */
+        var labelsMoisChrono = [];
+        var valeursMoisChrono = [];
+        for (let j = 11; j >= 0; j--) {
+            labelsMoisChrono.push(labelsMois[j]);
+            valeursMoisChrono.push(valeursMois[j]);
+        }
+
         var inscritsParMois = {
-            labels: ["Janvier", "Février", "Mars", "Avril", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"], 
-            values: [60, 10, 65, 20, 71, 28, 56, 37, 42, 25, 48] 
+            labels: labelsMoisChrono, 
+            values: valeursMoisChrono
         };
 
         return res.json(inscritsParMois);
